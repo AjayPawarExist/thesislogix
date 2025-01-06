f9b9b0b83c518146fe3f063bf0ffa437c5bcae0c
Riyaa745 <riyasainwal123@gmail.com>
2025-01-06 12:56:28 +0530

    contact home page complete

--- src/app/components/Contact.jsx
+++ src/app/components/Contact.jsx
@@ -3,14 +3,22 @@
 export default function Contact() {
   return (
     <div className="w-full py-8 bg-white text-black text-center">
-                <div>
-                    <h1 className="text-4xl font-semibold tracking-wider">Get Started</h1>
-                    <p className="text-[4fem] tracking-wide text-slate-500 mt-3">Contact us for personalized assistance today.</p>
-                </div>
-                <button className="bg-blue-950 text-white  mt-10  py-3 px-7 hover:bg-slate-700 ">Contact Us</button>
-            </div>
+      {/* Wrapper for Title and Subtitle */}
+      <div className="max-w-2xl mx-auto">
+        <h1 className="font-semibold tracking-wide sm:text-3xl md:text-4xl">
+          Get Started
+        </h1>
+        <p className=" py-3 tracking-wide text-slate-500 sm:text-xs md:text-sm ">
+          Contact us for personalized assistance today.
+        </p>
+      </div>
+
+      {/* Button for Contact Us */}
+      <button className="bg-blue-950 text-white font-light  mt-5 py-2 px-5 rounded hover:bg-slate-700 sm:text-xs md:text-sm lg:text-[11px]">
+        Contact Us
+      </button>
+    </div>
   )
 }
 
 
-// jhgjh
