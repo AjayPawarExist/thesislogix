ee5368f039045a82a47dd865e59805c832367c23
Ajay Pawar <ajaypawarexist@gmail.com>
2025-03-02 18:34:13 +0530

    Update Whatsappicon.jsx

--- src/app/components/Whatsappicon.jsx
+++ src/app/components/Whatsappicon.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const Whatsappicon = () => {
   return (
     <a
-      href="https://wa.me/7453851791" // Replace with your WhatsApp number
+      href="https://wa.me/9691815989" // Replace with your WhatsApp number
       target="_blank"
       className="fixed bottom-2 right-5 w-12 h-12 bg-green-500 rounded shadow-lg flex items-center justify-center hover:scale-110 transition-transform duration-300"
       title="Chat with us on WhatsApp"
