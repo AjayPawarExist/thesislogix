5e6a561c4fc837b39a72cc1723413cbc5a4ce99c
Ajay Pawar <ajaypawarexist@gmail.com>
2025-04-11 22:34:58 +0530

    upp

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,6 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: '/',
-      disallow: '/_not-found',
     },
     sitemap: 'https://thesislogix.com/sitemap.xml',
   }
