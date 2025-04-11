81da48571bffc599fd8c7b3fd746c94950378fd0
Ajay Pawar <ajaypawarexist@gmail.com>
2025-04-11 22:20:36 +0530

    upp

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,20 +1,27 @@
 import type { MetadataRoute } from 'next'
- 
+
+const pages = [
+  '/',
+  '/aboutus',
+  '/book',
+  '/contact',
+  '/implementation',
+  '/pattern',
+  '/plagiarism',
+  '/publication',
+  '/question',
+  '/rewriting',
+  '/synopsis',
+  '/thesisanalysis',
+  '/thesiswriting',
+  '/topicmakings',
+]
+
 export default function sitemap(): MetadataRoute.Sitemap {
-  return [
-    { url: '/', lastModified: new Date() },
-    { url: '/aboutus', lastModified: new Date() },
-    { url: '/book', lastModified: new Date() },
-    { url: '/contact', lastModified: new Date() },
-    { url: '/implementation', lastModified: new Date() },
-    { url: '/pattern', lastModified: new Date() },
-    { url: '/plagiarism', lastModified: new Date() },
-    { url: '/publication', lastModified: new Date() },
-    { url: '/question', lastModified: new Date() },
-    { url: '/rewriting', lastModified: new Date() },
-    { url: '/synopsis', lastModified: new Date() },
-    { url: '/thesisanalysis', lastModified: new Date() },
-    { url: '/thesiswriting', lastModified: new Date() },
-    { url: '/topicmakings', lastModified: new Date() },
-  ]
+  const lastModified = new Date()
+
+  return pages.map((url) => ({
+    url,
+    lastModified,
+  }))
 }
