5b44c7a18a8cbd7b8145ab9ab9990eef481017c9
Ajay Pawar <ajaypawarexist@gmail.com>
2025-04-11 22:38:55 +0530

    updated

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,27 +1,20 @@
 import type { MetadataRoute } from 'next'
-
-const pages = [
-  '/',
-  '/aboutus',
-  '/book',
-  '/contact',
-  '/implementation',
-  '/pattern',
-  '/plagiarism',
-  '/publication',
-  '/question',
-  '/rewriting',
-  '/synopsis',
-  '/thesisanalysis',
-  '/thesiswriting',
-  '/topicmakings',
-]
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  const lastModified = new Date()
-
-  return pages.map((url) => ({
-    url,
-    lastModified,
-  }))
-}
+  
+ export default function sitemap(): MetadataRoute.Sitemap {
+   return [
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/aboutus`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/book`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/implementation`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/pattern`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/plagiarism`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/publication`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/question`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/rewriting`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/synopsis`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/thesisanalysis`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/thesiswriting`, lastModified: new Date() },
+     { url: `${process.env.NEXT_PUBLIC_BASE_URL}/topicmakings`, lastModified: new Date() },
+   ]
+ }
